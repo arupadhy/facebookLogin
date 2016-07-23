c35e91a45f3ccb718e47d3b30cd7ff3dba982441
Arvind <aupadhyay12@sapient.com>
2016-07-22 22:19:50 -0400

    FB javascript sdk support for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var FB: any;
 
 @Component({
   moduleId: module.id,
@@ -6,6 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private login: boolean = false;
   title = 'app works!';
+  private loggedInUser:string;
+
+  public ngOnInit() {
+    this.login = window.localStorage.getItem("connected") ? true : false;
+  }
+  public facebookLogin() {
+    let ref: any = this;
+    FB.login(function (response) {
+      if (response.status === 'connected') {
+        console.log(response);
+        window.localStorage.setItem('connected', 'true');
+        ref.loggedInUser = response.id;
+        ref.fetchUserInfo();
+         ref.login = true;
+        document.getElementById('status').innerHTML = "connected to FB";
+      } else if (response.status === 'not_authorized') {
+        window.localStorage.clear();
+        document.getElementById('status').innerHTML = "Not authrized by fb...not logged in";
+        ref.login = false;
+      }
+    });
+  }
+
+  private fetchUserInfo() {
+    let ref:any = this; 
+    FB.api('/me', { fields: 'name,email,work' }, function (response) {  
+      console.log(response);
+      ref.loggedInUser = response.name;
+      console.log('loggedinUser set now');
+    });
+    //this.loggedInUser = "Neha";
+  }
 }
